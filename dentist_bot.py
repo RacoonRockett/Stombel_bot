a33cd3f30e78cd73834a5094d2709124c4510b56
RacoonRockett <154884354+RacoonRockett@users.noreply.github.com>
2025-05-31 13:09:27 +0300

    Update dentist_bot.py

--- dentist_bot.py
+++ dentist_bot.py
@@ -2,6 +2,22 @@ import sqlite3
 from openpyxl import Workbook
 from telegram import Update, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes, ConversationHandler
+from dotenv import load_dotenv
+import os
+import logging
+
+# --- Логирование ---
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# --- Загрузка токена ---
+load_dotenv()  # Только для локальной разработки
+
+TELEGRAM_TOKEN = os.getenv("STOMBOT_TOKEN")
+
+if TELEGRAM_TOKEN is None:
+    logger.error("Ошибка: TELEGRAM_BOT_TOKEN не найден в переменных окружения!")
+    raise EnvironmentError("Не указан STOMBOT_TOKEN")
 
 # Этапы диалога
 NAME, DATE, SERVICE, COST, PAID, EXPORT = range(6)
